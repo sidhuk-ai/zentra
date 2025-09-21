b0438ce78a0000edf5c4b62a079116b193f6130c
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-09-22 04:06:22 +0530

    feat: wrap children in AuthGuard within RootLayout for enhanced authentication handling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
 import { ClerkProvider } from "@clerk/nextjs"
+import { AuthGuard } from "@/module/auth/ui/components/auth-guard"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -25,7 +26,11 @@ export default function RootLayout({
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
         <ClerkProvider>
-          <Providers>{children}</Providers>
+          <Providers>
+            <AuthGuard>
+              {children}
+            </AuthGuard>
+          </Providers>
         </ClerkProvider>
       </body>
     </html>
