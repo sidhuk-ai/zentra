e856c74777c260c4db6adf10846161f5259e2bc3
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-09-21 09:37:15 +0530

    feat: created convex provider with clerk and wrapped around the main app

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,7 +2,9 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ConvexReactClient } from "convex/react"
+import { useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -14,9 +16,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      <ConvexProvider client={convex}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
-      </ConvexProvider>
+      </ConvexProviderWithClerk>
     </NextThemesProvider>
   )
 }
