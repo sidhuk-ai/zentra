5c59a664ae081380a2ebc4423315adb81407b188
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-09-23 01:45:36 +0530

    feat: added clerk user button to manage their account

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs"
 import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
@@ -5,6 +6,7 @@ export default function Page() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
+        <UserButton />
         <Button size="sm">Button</Button>
       </div>
     </div>
