5b69baeed9e80ef08cd1a68b4320015404e90f93
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-04 05:39:46 +0530

    refactor: wrapped all the dashboard related routes under <DashboardLayout> component

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,9 +1,10 @@
 import { AuthGuard } from "@/module/auth/ui/components/auth-guard";
+import { DashboardLayout } from "@/module/dashboard/ui/layouts/dashboard-layout";
 
 export default function Layout({ children }:{ children: React.ReactNode }) {
     return (
-        <AuthGuard>
+        <DashboardLayout>
             {children}
-        </AuthGuard>
+        </DashboardLayout>
     )
 }
