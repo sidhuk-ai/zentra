2488e1ab5e1f9cb4b0b0a61a05432b7eafcd06b7
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-04 05:38:42 +0530

    feat: created a dashboard layout for the (dashboard) routes and protected it with AuthGaurd and OrganizationGaurd

---
+++ apps/web/module/dashboard/ui/layouts/dashboard-layout.tsx
@@ -0,0 +1,22 @@
+import { AuthGuard } from "@/module/auth/ui/components/auth-guard";
+import { OrganizationGaurd } from "@/module/auth/ui/components/organization-gaurd";
+import { SidebarProvider } from "@workspace/ui/components/sidebar"
+import { cookies } from "next/headers";
+import { DashsboardSidebar } from "@/module/dashboard/ui/components/dashboard-sidebar";
+
+export async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const cookieStore = await cookies();
+    const defaultValue = cookieStore.get("sidebar_state")?.value === "true";
+    return (
+        <AuthGuard>
+            <OrganizationGaurd>
+                <SidebarProvider defaultOpen={defaultValue}>
+                    <DashsboardSidebar />
+                    <main className="flex flex-1 flex-col">
+                        {children}
+                    </main>
+                </SidebarProvider>
+            </OrganizationGaurd>
+        </AuthGuard>
+    );
+}
