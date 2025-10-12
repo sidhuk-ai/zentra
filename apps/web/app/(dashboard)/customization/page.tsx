f8e5257507d80f6615cda3fa273c1b6299c7853f
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-13 01:54:43 +0530

    feat: add new pages for billing, conversations, customization, files, integrations, and plugins in the dashboard

---
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <>
+        <h1>Widgets Customization Page</h1>
+        </>
+    )
+}
---
+++ apps/web/app/(dashboard)/integrations/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <>
+        <h1>Integration Page</h1>
+        </>
+    )
+}
