4a0e166e4bfcd18256439b00f44c443bba7c2cfc
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-13 04:03:56 +0530

    feat: replace placeholder content with ChatbotWidget for dynamic interaction

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { ChatbotWidget } from "@/modules/widgets/ui/widget-view";
+import { useSearchParams } from "next/navigation";
+
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const organizationId = searchParams.get("organizationId") || "";
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+      <ChatbotWidget organizationId={organizationId} />
     </div>
   )
 }
