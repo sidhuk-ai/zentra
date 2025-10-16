4406d1f9b711274439622eb0fa6434ab808c3bb8
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-16 06:22:22 +0530

    feat: integrate supportAgent to create conversation threads and save initial messages

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,5 +1,8 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "../_generated/server";
+import { supportAgent } from "../system/ai/agents/supportAgent";
+import { saveMessage } from "@convex-dev/agent";
+import { components } from "../_generated/api";
 
 export const getOne = query({
     args: {
@@ -49,7 +52,17 @@ export const create = mutation({
             })
         }
 
-        const threadId = "123";
+        const { threadId } = await supportAgent.createThread(ctx,{
+            userId: args.organizationId
+        });
+
+        await saveMessage(ctx,components.agent,{
+            threadId,
+            message: {
+                role: "assistant",
+                content: "Hello, how can I help you today?"
+            }
+        })
         const conversationId = await ctx.db.insert("conversation",{
             contactSessionId: session._id,
             organizationId: args.organizationId,
