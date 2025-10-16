08e07a11203af9efa219db000fee8d25ce9692e0
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-16 06:19:04 +0530

    fix: fixed the build error which was happening when useSearchParam() hook was not bounded by a <Suspense> boundary.

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { Suspense } from "react";
 import { ChatbotWidget } from "@/modules/widgets/ui/widget-view";
 import { useSearchParams } from "next/navigation";
 
-
-export default function Page() {
+function ChatbotPageContent() {
   const searchParams = useSearchParams();
   const organizationId = searchParams.get("organizationId") || "";
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <ChatbotWidget organizationId={organizationId} />
     </div>
-  )
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading widget...</div>}>
+      <ChatbotPageContent />
+    </Suspense>
+  );
 }
