aff54b97537bb43c37ccb6b2023402a6b6fc6cea
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-24 22:04:54 +0530

    feat: implement escalate and resolve conversation tools for improved support interactions

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,9 +1,10 @@
 import { google } from "@ai-sdk/google";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api"
+import { SUPPORT_AGENT_PROMPT } from "../../../../constant"
 
 export const supportAgent = new Agent(components.agent, {
     name: "Support Agent",
     languageModel: google.chat("gemini-2.5-flash"),
-    instructions: "You are a customer support agent"
+    instructions: SUPPORT_AGENT_PROMPT,
 });
