338241e691c44d86371e98d99199ae92303c9ba7
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-10-27 11:33:02 +0530

    feat: add listFile query for retrieving files with pagination and category filtering

--- packages/backend/convex/private/files.ts
+++ packages/backend/convex/private/files.ts
@@ -1,9 +1,10 @@
 import { ConvexError, v } from "convex/values";
-import { action, mutation } from "../_generated/server";
-import { contentHashFromArrayBuffer, guessMimeTypeFromContents, guessMimeTypeFromExtension, vEntryId } from "@convex-dev/rag";
+import { action, mutation, query, QueryCtx } from "../_generated/server";
+import { contentHashFromArrayBuffer, Entry, EntryId, guessMimeTypeFromContents, guessMimeTypeFromExtension, vEntryId } from "@convex-dev/rag";
 import { extractTextContent } from "../lib/extractTextContent";
 import rag from "../system/ai/rag";
 import { Id } from "../_generated/dataModel";
+import { paginationOptsValidator } from "convex/server";
 
 function guessMimeType(filename: string, bytes: ArrayBuffer): string {
     return (
@@ -55,7 +56,7 @@ export const addFile = action({
                 uploadedBy: orgId,
                 filename,
                 category: category ?? null
-            },
+            } as EntryMetadata,
             contentHash: await contentHashFromArrayBuffer(bytes) // TODO: learn about this
         });
 
@@ -97,7 +98,7 @@ export const deleteFile = mutation({
         if(!namespace) {
             throw new ConvexError({
                 code: "UNAUTHORIZED",
-                message: "Inavlid namesapce"
+                message: "Invalid namespace"
             });
         }
 
@@ -125,3 +126,117 @@ export const deleteFile = mutation({
         });
     }
 })
+
+export const listFile = query({
+    args: {
+        category: v.optional(v.string()),
+        paginationOpts: paginationOptsValidator
+    },
+    handler: async (ctx,args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (identity === null) {
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Identity not found"
+            });
+        }
+
+        const orgId = identity.orgId as string;
+        if (!orgId) {
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Organization not found"
+            });
+        }
+
+        const namespace = await rag.getNamespace(ctx,{
+            namespace: orgId
+        });
+        if(!namespace) {
+            return {page: [], isDone: true, continueCursor: ""};
+        }
+
+        const results = await rag.list(ctx, {
+            namespaceId: namespace.namespaceId,
+            paginationOpts: args.paginationOpts
+        });
+
+        const files = await Promise.all(
+            results.page.map((entry) => convertEntryToPublicFile(ctx,entry))
+        );
+        const filteredFile = args.category ? files.filter((file) => file.category === args.category) : files;
+
+        return {
+            page: filteredFile,
+            continueCursor: results.continueCursor,
+            isDone: results.isDone
+        }
+    }
+});
+
+export type PublicFile = {
+    id: EntryId;
+    name: string;
+    type: string;
+    size: string;
+    status: "ready" | "processing" | "error";
+    url: string | null;
+    category?: string
+}
+type EntryMetadata = {
+    storageId: Id<"_storage">;
+    uploadedBy: string;
+    filename: string;
+    category?: string | null;
+}
+
+async function convertEntryToPublicFile(ctx:QueryCtx, entry: Entry): Promise<PublicFile> {
+    const metadata = entry.metadata as EntryMetadata || undefined;
+    const storageId = metadata?.storageId;
+    let fileSize = "unknown";
+
+    if(storageId) {
+        try {
+            const storageMetadata = await ctx.db.system.get(storageId);
+            if(storageMetadata) {
+                fileSize = fileSizeFormatter(storageMetadata.size);
+            }
+        } catch (error) {
+            console.error("Failed to get storage metadata", error);
+        }        
+    }
+
+    const filename = entry.key || "Unknown";
+    const extension = filename.split(".").pop()?.toLowerCase() || "txt"
+
+    let status: "ready" | "processing" | "error" = "error";
+    if(entry.status === "ready") {
+        status = "ready";
+    } else if(entry.status === "pending") {
+        status = "processing"
+    }
+
+    const url = storageId ? await ctx.storage.getUrl(storageId) : null;
+
+    return {
+        id: entry.entryId,
+        name: filename,
+        type: extension,
+        size: fileSize,
+        status,
+        url,
+        category: metadata?.category || undefined
+    }
+};
+
+function fileSizeFormatter(byteSize:number): string {
+    if(byteSize === 0) {
+        return "0 B";
+    }
+    const kB = 1024;
+    const sizes = ["B","KB","MB","GB"];
+    const i = Math.floor(Math.log(byteSize)/Math.log(kB));
+
+    return `${Number.parseFloat((byteSize/kB ** i).toFixed(1))} ${sizes[i]}`;
+}
