2a6f9ec92aad9ee43ddc1769bc384a4e84d92454
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-11-19 00:09:21 +0530

    feat: create Page component with HeroSection and FeaturesSection

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import FeaturesSection from "@/components/landing/features";
+import HeroSection from "@/components/landing/hero-section";
+
+export default function Page() {
+    return (
+        <>
+        <HeroSection />
+        <FeaturesSection />
+        </>
+    )
+}
